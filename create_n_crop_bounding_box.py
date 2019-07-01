e901322c5ab4911d15e8a9cfbb4afcb5c1199308
sua474 <sua474@mail.usask.ca>
2019-06-30 20:42:46 -0600

    create and crop bounding box .py file for easy import

---
+++ create_n_crop_bounding_box.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import skimage.io as io
+import matplotlib.patches as pt
+import os as os
+import sys
+from math import ceil
+
+def show_bounding_boxes(dir_name="Umair"):
+    
+    #Parameter:dir_name assumes that both image and annotation directories are in your pwd
+    #The function takes the direcotry name and creates bounding boxes around the annoted 
+    #clumped plants.
+    
+    
+    images_path = os.path.join('.', dir_name)
+    
+    if not os.path.exists(images_path):
+        print("Directory Doesn't Exist\nExiting the Program")
+
+    for root, dirs, files in os.walk(images_path):
+        for filename in files:
+            image = io.imread(root+'//'+filename)
+            annotations_file = open(dir_name+'_annotations'+'/'+filename.replace(".JPG",".txt"),"r")
+            fig = plt.figure(figsize=(10,10))
+            boxes = annotations_file.readlines()
+            for box in boxes:
+                coordinates = np.array(box.split())
+                coordinates = coordinates.astype(np.float)
+                x = (coordinates[1] - coordinates[3]/2)*image.shape[1]
+                y = (coordinates[2] - coordinates[4]/2)*image.shape[0]
+                rect = pt.Rectangle((x,y),coordinates[3]*image.shape[1],coordinates[4]*image.shape[0],linewidth=2,edgecolor='y',facecolor='none')
+                ax2 = fig.add_subplot(111, aspect='equal')
+                ax2.add_patch(rect)
+            plt.imshow(image)
+
+def cropped_bounding_boxes(dir_name='Umair',filename='IMG_4426.JPG'):
+    
+    #Parameter:dir_name is the name of the directory
+    #Parameter:filename is the name of the image file
+    #returns: cropped images array and the class_label array
+    #The class_label_array will represent the TRUE class of same image on corresponding cropped_image array
+    
+    try:
+        image = io.imread(dir_name+'//'+filename)
+        annotations_file = open(dir_name+'_annotations'+'/'+filename.replace(".JPG",".txt"),"r")
+    except:
+        sys.exit("Cannot Find the specified direcotry/Image")
+    
+    boxes = annotations_file.readlines()
+    cropped_images=[]
+    image_labels = []
+    fig = plt.figure(figsize=(10,10))
+    for box in boxes:
+        coordinates = np.array(box.split())
+        coordinates = coordinates.astype(np.float)
+        x = (coordinates[1] - coordinates[3]/2)*image.shape[1]
+        y = (coordinates[2] - coordinates[4]/2)*image.shape[0]
+        rect = pt.Rectangle((x,y),coordinates[3]*image.shape[1],coordinates[4]*image.shape[0],linewidth=2,edgecolor='y',facecolor='none')
+        cropped_images.append(image[ceil(y):ceil(y+coordinates[4]*image.shape[0]),ceil(x):ceil(x+coordinates[3]*image.shape[1]),])
+        image_labels.append(int(coordinates[0]+1))
+        ax2 = fig.add_subplot(111, aspect='equal')
+        ax2.add_patch(rect)
+    plt.title("Orignial Image with Bounding Boxes")
+    plt.imshow(image)
+    return cropped_images,image_labels
+
+
+# Function Call Test
+#cropped_images,image_labels = cropped_bounding_boxes('Umair','IMG_4456.JPG')
+#for i in range(0,len(cropped_images)):
+#    fig = plt.figure()
+#    plt.title("Clump Image: "+str(i+1)+" with: "+str(image_labels[i])+" Plants")
+#    plt.imshow(cropped_images[i])
