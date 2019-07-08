d925a4fec0469bdf715811515be568c7f1949a7b
kts135 <25314187+KyleS22@users.noreply.github.com>
2019-07-08 10:21:26 -0600

    Tried using randm weights

--- src/VGG_for_counting/model.py
+++ src/VGG_for_counting/model.py
@@ -44,7 +44,7 @@ import shutil
 
 
 # Get the pretrained VGG weights
-vgg16 = VGG16(weights='imagenet', include_top=True)
+vgg16 = VGG16(weights=None, include_top=True)
 
 # Add some fully connected layers
 #x = Flatten(name='flatten')(vgg16.output)
@@ -52,7 +52,7 @@ vgg16 = VGG16(weights='imagenet', include_top=True)
 #x = Dense(4096, activation='relu', name='fc2')(x)
 
 # 20 is currently an arbitrary number of classes, representing clumps with a number of plants from 1-20
-x = Dense(20, activation='softmax', name='predictions')(vgg16.layers[-2].output)
+x = Dense(4, activation='softmax', name='predictions')(vgg16.layers[-2].output)
 
 # Create the model
 model = Model(input=vgg16.input, output=x)
@@ -76,29 +76,24 @@ except:
     raise Exception("Could not make train and test dirs")
 
 # This is the set of images to ignore, so we can see if the model can generalize to a label it has not seen
-label_to_ignore = "6"
+
+labels_to_use = ["2", "3", "4", "5"]
 
 images = []
 image_labels = []
 
 for label in labels:
     
-    print(label)
-    if label == label_to_ignore:
-        for img in os.listdir(os.path.join(data_dir, label)):
-            shutil.copy(os.path.join(data_dir,label, img), os.path.join(test_dir, label))
-
-    else:
+    if label in labels_to_use:
         for image in os.listdir(os.path.join(data_dir, label)):
             images.append(image)
             image_labels.append(label)
 
-os.mkdir(os.path.join(train_dir, label_to_ignore))
 
-for i in range(20):
-    if not str(i) in labels:
-        os.mkdir(os.path.join(train_dir, str(i)))
-        os.mkdir(os.path.join(test_dir, str(i)))
+#for i in range(20):
+#    if not str(i) in labels:
+#        os.mkdir(os.path.join(train_dir, str(i)))
+#        os.mkdir(os.path.join(test_dir, str(i)))
 
 X_train, X_test, y_train, y_test = train_test_split(images, image_labels, test_size=0.2, random_state=42)
 
@@ -124,7 +119,7 @@ for image, label in zip(X_test, y_test):
 
 
 # Load the training Data
-BATCH_SIZE = 20
+BATCH_SIZE = 5
 
 train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, validation_split=0.2)
 
@@ -134,10 +129,35 @@ validation_generator = train_datagen.flow_from_directory(train_dir, target_size=
 # Train 
 #model.fit(X_train, y_train, validation_data=(X_val, y_val))
 model.fit_generator(train_generator, steps_per_epoch = train_generator.samples // BATCH_SIZE, validation_data =
-    validation_generator, validation_steps = validation_generator.samples // BATCH_SIZE, epochs = 15)
+    validation_generator, validation_steps = validation_generator.samples // BATCH_SIZE, epochs = 30)
 
+test_labels = os.listdir(test_dir)
+
+print(test_labels)
+
+test_images = []
+test_image_labels = []
+
+for label in test_labels:
+    for image in os.listdir(os.path.join(test_dir, label)):
+            test_images.append(image)
+            test_image_labels.append(label)
+
+
+for img, label in zip(test_images, test_image_labels):
+
+    image = load_img(os.path.join(test_dir, label, img), target_size=(224, 224))
+## convert the image pixels to a numpy array
+    image = img_to_array(image)
+## reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    
+    prediction = model.predict_classes(image)
+
+    print(prediction, label)
 
 # Test it
+
 #predictions = model.predict(X_test)
 
 # TODO: Compare to y_test
